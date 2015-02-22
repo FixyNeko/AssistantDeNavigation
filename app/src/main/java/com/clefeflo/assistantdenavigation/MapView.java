7a1aed4dfc2409ea618298d38a523c38cbb6a289
FixyNeko <felix@dhumes.fr>
2015-02-22 19:08:08 +0100

    tentative d'affichage d'une image de fond. Création de certaines méthodes/ ajustements pour afficher les trajets

--- app/src/main/java/com/clefeflo/assistantdenavigation/MapView.java
+++ app/src/main/java/com/clefeflo/assistantdenavigation/MapView.java
@@ -7,6 +7,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Picture;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -17,34 +20,38 @@ public class MapView extends View {
     Paint paint = new Paint();
     Canvas floor0 = new Canvas(), floor1 = new Canvas(), floor2 = new Canvas(), floor3 = new Canvas();
     Bitmap bmp;
-    Picture pic = new Picture();
+    Thread t;
 
     public MapView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        final Thread t = new Thread() {
+        t = new Thread() {
             @Override
             public void run() {
-                paint.setColor(Color.YELLOW);
+                paint.setColor(Color.BLACK);
                 paint.setStyle(Paint.Style.STROKE);
                 paint.setStrokeWidth(10);
-                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ifloor0);
-                floor0.drawBitmap(bmp, 0,0, null);
-                pic.draw(floor0);
-                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ifloor0);
-                floor1.drawBitmap(bmp, 0,0, null);
-                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ifloor0);
-                floor2.drawBitmap(bmp, 0,0, null);
-                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ifloor0);
-                floor3.drawBitmap(bmp, 0,0, null);
+//                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ifloor0);
+//                floor0.drawBitmap(bmp, 500, 500, null);
+//                floor0.drawLine(0,0,500,500,paint);
+//                pic.draw(floor0);
+
+                bmp = ((BitmapDrawable) getResources().getDrawable(R.drawable.ifloor0)).getBitmap();
+                System.out.println("bmp défini");
             }
         };
         t.start();
     }
 
     public void onDraw(Canvas canvas) {
-        canvas.drawPicture(pic);
-
+        canvas.drawLine(0, 50, 200, 250, paint);
+        while(t.isAlive()) {
+            try {
+                canvas.drawBitmap(bmp, 0, 0, paint);
+            } catch (Exception e) {
+                System.out.println("Bitmap not printed: " + e);
+            }
+        }
     }
 
     public static void draw(int[] pathNums) {
